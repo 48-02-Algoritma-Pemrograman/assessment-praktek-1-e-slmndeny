4ba011e4f1ac8d1e878786e9eaecbae8fb8f5bc9
slmndeny <139955071+slmndeny@users.noreply.github.com>
2024-10-24 14:11:49 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Nomor1{
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        int jumlahAnggota, tunjangan; 
 
+        System.out.println("\nJumlah Anggota Keluarga: ");
+        jumlahAnggota = input.nextInt();
+
+        if (jumlahAnggota <=3 ) {
+            tunjangan = jumlahAnggota * 100000;
+        }
+        else {
+            tunjangan = (3 * 100000) + ((jumlahAnggota - 3) * 50000);
+        }
+
+        System.out.println("Besar tunjangan kesehatan= Rp " + tunjangan);
+
+        input.close();
     }
 }
